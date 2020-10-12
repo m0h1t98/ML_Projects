2a3cad35dc4375d5f40c32221122f9a3b5c7cfff
Mohit Agrawal <52734794+m0h1t98@users.noreply.github.com>
2020-10-12 19:06:22 +0530

    Add files via upload

---
+++ Fake News Detection/App/feature.py
@@ -0,0 +1,21 @@
+import numpy as np # linear algebra
+import pandas as pd #data processing
+
+import os
+import re
+import nltk
+
+def get_all_query(title, author, text):
+    total= title + author + text
+    total = [total]
+    return total
+
+def remove_punctuation_stopwords_lemma(sentence):
+    filter_sentence = ''
+    lemmatizer=WordNetLemmatizer()
+    sentence = re.sub(r'[^\w\s]','',s)
+    words = nltk.word_tokenize(sentence) #tokenization
+    words = [w for w in words if not w in stop_words]
+    for word in words:
+        filter_sentence = filter_sentence + ' ' + str(lemmatizer.lemmatize(word)).lower()
+    return filter_sentence
